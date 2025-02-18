b8eddfdecaf4fb627907b9fb519b94aa2cac8977
nachomallavia <nachomallavia@hotmail.com>
2025-02-18 16:54:49 -0300

    Added Allow CORS

--- index.js
+++ index.js
@@ -12,6 +12,15 @@ const API_TOKEN = process.env.API_TOKEN;
 // Enable CORS
 app.use(cors());
 
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header(
+        'Access-Control-Allow-Headers',
+        'Origin, X-Requested-With, Content-Type, Accept'
+    );
+    next();
+});
+
 // Route to handle ID parameter
 app.get('/:id', async (req, res) => {
     try {
